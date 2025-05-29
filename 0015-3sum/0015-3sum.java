639a46630178c2368821d7832d1335d97409c9dd
tanixhqyadav <122175355+tanixhqyadav@users.noreply.github.com>
2025-05-29 23:56:27 +0530

    Time: 31 ms (58.39%), Space: 51.6 MB (60.04%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,33 +1,34 @@
-import java.util.*;
-
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> ll=new ArrayList<>();
         Arrays.sort(nums);
-        Set<List<Integer>> set = new HashSet<>();
-        
-        for (int i = 0; i < nums.length; i++) {
-            int op = nums[i];
-            int left = i + 1;
-            int right = nums.length - 1;
-            
-            while (left < right) {
-                int sum = op + nums[left] + nums[right];
-                
-                if (sum == 0) {
-                    List<Integer> triplet = Arrays.asList(op, nums[left], nums[right]);
-                    set.add(triplet); 
-                    left++;
-                    right--;
-                } 
-                else if (sum < 0) {
-                    left++;
-                } 
-                else {
-                    right--;
+        for(int i=0;i<nums.length-2;i++){
+            if(i>0 && nums[i-1]==nums[i]){
+                continue;
+            }
+            int j=i+1;
+            int l=nums.length-1;
+            while(j<l){
+                int s=nums[i]+nums[j]+nums[l];
+                if(nums[i]+nums[j]+nums[l]==0){
+                    ll.add(Arrays.asList(nums[i],nums[j],nums[l]));
+                    while(j<l && nums[j]==nums[j+1]){
+                        j++;
+                    }
+                    while(j<l && nums[l-1]==nums[l]){
+                        l--;
+                    }
+                    j++;
+                    l--;
+                }
+                else if(s<0){
+                    j++;
+                }
+                else{
+                    l--;
                 }
             }
         }
-        
-        return new ArrayList<>(set); 
+        return ll;
     }
 }
