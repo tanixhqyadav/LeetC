e39bcd065252f109df27cbd842ee108a8a8d0c71
tanixhqyadav <122175355+tanixhqyadav@users.noreply.github.com>
2025-06-06 17:11:27 +0530

    Time: 0 ms (100%), Space: 41.4 MB (64.74%) - LeetHub

---
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.java
@@ -0,0 +1,27 @@
+class MyStack {
+    Queue<Integer> q;
+
+    public MyStack() {
+        q=new LinkedList<>();
+    }
+
+    public void push(int x) {
+        q.offer(x);
+        int size = q.size();
+        while (size-->1){
+            q.offer(q.poll());
+        }
+    }
+
+    public int pop() {
+        return q.poll();
+    }
+
+    public int top() {
+        return q.peek();
+    }
+
+    public boolean empty() {
+        return q.isEmpty();
+    }
+}
