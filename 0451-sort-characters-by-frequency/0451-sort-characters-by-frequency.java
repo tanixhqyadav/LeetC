822cc162efa4b29d249a7a7ab28aa3c23082d89c
tanixhqyadav <122175355+tanixhqyadav@users.noreply.github.com>
2025-06-28 16:53:47 +0530

    Time: 420 ms (5.38%), Space: 46.3 MB (9.92%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
@@ -1,36 +1,34 @@
 class Pair{
     char ch;
     int frq;
-    Pair(char ch , int frq){
+    Pair(char ch, int frq){
         this.ch=ch;
         this.frq=frq;
     }
 }
 class Solution {
     public String frequencySort(String s) {
-        HashMap<Character,Integer> mp=new HashMap<>();
+        HashMap<Character, Integer> mp=new HashMap<>();
         for(int i=0;i<s.length();i++){
             mp.put(s.charAt(i),mp.getOrDefault(s.charAt(i),0)+1);
         }
-        List<Pair> l=new ArrayList<>();
-        // PriorityQueue<Character> pq=new PriorityQueue<>();
-        for(Character ch:mp.keySet()){
-            l.add(new Pair(ch,mp.get(ch)));
-        }
-        Collections.sort(l, (a, b) -> {
-            if (a.frq == b.frq) {
-            return Character.compare(a.ch, b.ch);
+        PriorityQueue<Pair> pq=new PriorityQueue<>((a,b)->{
+            if(mp.get(a.ch)==mp.get(b.ch)){
+                return Character.compare(a.ch,b.ch);
+            }else{
+                return Integer.compare(mp.get(b.ch),mp.get(a.ch));
             }
-            return Integer.compare(b.frq, a.frq); 
         });
+        for(Character ch: mp.keySet()){
+            pq.add(new Pair(ch,mp.get(ch)));
+        }
         String ans="";
-        for(int i=0;i<l.size();i++){
-            Pair x=l.get(i);
-            int fr=x.frq;
-            char c=x.ch;
-            while(fr!=0){
-                ans+=c;
-                fr--;
+        while(!pq.isEmpty()){
+            Pair p=pq.poll();
+            char jk=p.ch;
+            int fk=p.frq;
+            while(fk-->0){
+                ans+=jk;
             }
         }
         return ans;
