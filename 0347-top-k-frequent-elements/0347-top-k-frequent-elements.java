3d14c442b8a7917de6753fa98eee3086ba2b7a65
tanixhqyadav <122175355+tanixhqyadav@users.noreply.github.com>
2025-06-28 16:17:12 +0530

    Time: 13 ms (76.36%), Space: 45.5 MB (94.93%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,23 +1,23 @@
 class Solution {
-    public int[] topKFrequent(int[] ar, int k) {
+    public int[] topKFrequent(int[] nums, int k) {
         HashMap<Integer, Integer> mp=new HashMap<>();
-        for(int i=0;i<ar.length;i++){
-            mp.put(ar[i], mp.getOrDefault(ar[i], 0) + 1);
+        for(int i=0;i<nums.length;i++){
+            mp.put(nums[i],mp.getOrDefault(nums[i],0)+1);
         }
-        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> mp.get(b) - mp.get(a));
-        // for(Integer ke : mp.keySet()){
-        //     if(!pq.isEmpty() && mp.get(pq.peek())>=mp.get(ke)){
-        //         pq.poll();
-        //         pq.add(ke);
-        //     }
-        //     pq.add(ke);
-        // }
-        pq.addAll(mp.keySet());
-
-        int ari[]=new int[k];
+        PriorityQueue<Integer> pq=new PriorityQueue<>( (a,b) ->{
+            if(mp.get(a)==mp.get(b)){
+                return Integer.compare(a,b);
+            }else{
+                return mp.get(b) - mp.get(a);
+            }
+            });
+        for(Integer i:mp.keySet()){
+            pq.add(i);
+        }
+        int ar[]=new int[k];
         for(int i=0;i<k;i++){
-            ari[i]=pq.poll();
+            ar[i]=pq.poll();
         }
-        return ari;
+        return ar;
     }
 }
