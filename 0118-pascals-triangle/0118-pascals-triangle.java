15061d3b1085e18be38eea0c5f25b0aaea1c5ead
tanixhqyadav <122175355+tanixhqyadav@users.noreply.github.com>
2025-08-01 18:18:26 +0530

    Time: 1 ms (82.66%), Space: 42 MB (79.28%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.java
+++ 0118-pascals-triangle/0118-pascals-triangle.java
@@ -1,16 +1,40 @@
+
 class Solution {
-    public List<List<Integer>> generate(int n) {
+    public List<List<Integer>> generate(int num) {
         List<List<Integer>> ll=new ArrayList<>();
-        for(int i=0;i<n;i++){
-            int c=1;
-            List<Integer> l=new ArrayList<>();
-            for(int j=0;j<=i;j++){
-                l.add(c);
-                c=c*(i-j)/(j+1);
+        ll.add(Arrays.asList(1));
+        if(num==1){
+            return ll;
+        }
+        ll.add(Arrays.asList(1,1));
+        if(num==2){
+            return ll;
+        }
+        for(int i=3;i<=num;i++){
+            List<Integer> l= new ArrayList<>();
+            l.add(1);
+            List<Integer> op=ll.get(ll.size()-1);
+            int s=0;
+            for(int j=0;j<op.size()-1;j++){
+                s=op.get(j)+op.get(j+1);
+                l.add(s);
+                s=0;
             }
+            l.add(1);
             ll.add(l);
         }
-        System.out.println(ll);
         return ll;
     }
+
+    // public static long fact(int n) {
+    //     long p = 1;
+    //     for (int i = 1; i <= n; i++) {
+    //         p *= i;
+    //     }
+    //     return p;
+    // }
+
+    // public static long com(int f, int r) {
+    //     return fact(f) / (fact(f - r) * fact(r));
+    // }
 }
