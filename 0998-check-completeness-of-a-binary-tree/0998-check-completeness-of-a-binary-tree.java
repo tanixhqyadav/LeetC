ccb7bbddd4dda11eac3d951347067b867e295930
tanixhqyadav <122175355+tanixhqyadav@users.noreply.github.com>
2025-08-03 19:14:17 +0530

    Time: 8 ms (1.6%), Space: 42.9 MB (6.57%) - LeetHub

--- 0998-check-completeness-of-a-binary-tree/0998-check-completeness-of-a-binary-tree.java
+++ 0998-check-completeness-of-a-binary-tree/0998-check-completeness-of-a-binary-tree.java
@@ -13,71 +13,36 @@
 //  *     }
 //  * }
 //  */
-//  class Pair{
-//     TreeNode p;
-//     int ins;
-//     Pair(TreeNode p, int ins){
-//         this.p=p;
-//         this.ins=ins;
-//     }
-//  }
-// class Solution {
-//     public boolean isCompleteTree(TreeNode root) {
-//         Queue<Pair> q=new LinkedList();
-//         q.add(new Pair(root,0));
-//         int c=0;
-//         int idx=0;
-//         while(!q.isEmpty()){
-//             Pair l=q.poll();
-//             TreeNode op=l.p;
-//             int kl=l.ins;
-//             idx=kl;
-//             c++;          
-//             if(op.left!=null){
-//                 q.add(new Pair(op.left,2*kl+1));
-//                 // l.add(op.left.val);
-//             }
-//             if(op.right!=null){
-//                 q.add(new Pair(op.left,2*kl+2));
-//                 // l.add(op.right.val);
-//             }
-//         }
-//         System.out.println(idx+" "+c);
-//         return c==idx;
-//     }
-// }
-class Pair {
+ class Pair{
     TreeNode p;
     int ins;
-    Pair(TreeNode p, int ins) {
-        this.p = p;
-        this.ins = ins;
+    Pair(TreeNode p, int ins){
+        this.p=p;
+        this.ins=ins;
     }
-}
-
+ }
 class Solution {
     public boolean isCompleteTree(TreeNode root) {
-        Queue<Pair> q = new LinkedList<>();
-        q.add(new Pair(root, 0));
-        int c = 0;
-        int idx = 0;
-
-        while (!q.isEmpty()) {
-            Pair l = q.poll();
-            TreeNode op = l.p;
-            int kl = l.ins;
-            idx = kl;
-            c++;
-
-            if (op.left != null) {
-                q.add(new Pair(op.left, 2 * kl + 1));
+        Queue<Pair> q=new LinkedList();
+        q.add(new Pair(root,0));
+        int c=0;
+        int idx=0;
+        while(!q.isEmpty()){
+            Pair l=q.poll();
+            TreeNode op=l.p;
+            int kl=l.ins;
+            idx=kl;
+            c++;          
+            if(op.left!=null){
+                q.add(new Pair(op.left,2*kl+1));
+                // l.add(op.left.val);
             }
-            if (op.right != null) {
-                q.add(new Pair(op.right, 2 * kl + 2)); 
+            if(op.right!=null){
+                q.add(new Pair(op.right,2*kl+2));
+                // l.add(op.right.val);
             }
         }
-
-        System.out.println(idx +"" + c);
-        return c == idx + 1;
+        System.out.println(idx+" "+c);
+        return c==idx+1;
     }
 }
