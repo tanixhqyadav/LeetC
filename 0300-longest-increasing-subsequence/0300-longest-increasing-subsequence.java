d1657d1b00e8749ae8019bad8b1bda37b7dc2ba2
tanixhqyadav <122175355+tanixhqyadav@users.noreply.github.com>
2025-09-11 17:29:19 +0530

    Time: 50 ms (39.32%), Space: 43.9 MB (95.26%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,51 +1,18 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
-        int dp[]=new int[nums.length];
-        Arrays.fill(dp,1);
-        for(int i=1;i<nums.length;i++){
-            for(int j=0;j<i;j++){
-                if(nums[i]>nums[j]){
-                    dp[i]=Math.max(dp[i],dp[j]+1);
-                }
+    int dp[]=new int[nums.length];
+    Arrays.fill(dp,1);
+    for(int i=1;i<dp.length;i++){
+        for(int j=0;j<i;j++){
+            if(nums[i]>nums[j]){
+                dp[i]=Math.max(dp[i],dp[j]+1);
             }
         }
-        int max=1;
-        for(int i:dp){
-            System.out.print(i+" ");
-            max=Math.max(i,max);
-        }
-        return max;
     }
-    public static int longo_Binary(int ar[]){
-        int dp[]=new int[ar.length];
-        dp[0]=ar[0];
-        int le=1;
-        for(int i=1;i<dp.length;i++){
-            if(ar[i]>dp[le-1]){
-                dp[le]=ar[i];
-                le++;
-            }
-            else{
-                int idx=Binary_s(dp,0,le-1,ar[i]);
-                dp[idx]=ar[i];
-            }
-        }
-        return le;
+    int max=1;
+    for(int l:dp){
+        max=Math.max(l,max);
     }
-    public static int Binary_s(int dp[], int i, int j, int x){
-        int a=i;
-        int b=j;
-        int ans=0;
-        while(a<=b){
-            int mid=(a+b)/2;
-            if(dp[mid]>=x){
-                ans=mid;
-                b=mid-1;
-            }
-            else{
-                a=mid+1;
-            }
-        }
-        return ans;
+    return max;
     }
 }
