aa3dcd0e765aec99e74dc7fb6c1648f18025c4be
tanixhqyadav <122175355+tanixhqyadav@users.noreply.github.com>
2025-09-25 23:46:11 +0530

    Time: 4 ms (30.23%), Space: 46.1 MB (60.97%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,44 +1,19 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int nums3[] = new int[nums1.length+nums2.length];
+        int nums3[]=new int[nums1.length+nums2.length];
+        int x=0;
         for(int i=0;i<nums1.length;i++){
-            // double k = nums1[i];
-            nums3[i] = nums1[i];
+            nums3[x]=nums1[i];
+            x++;
         }
-
-        for(int j=0;j<nums2.length;j++){
-            // double h = nums2[j];
-            nums3[nums1.length+j] = nums2[j];
+        for(int i=0;i<nums2.length;i++){
+            nums3[x]=nums2[i];
+            x++;
         }
-
-        Arrays.sort(nums3);    
-
-        int mid = (nums3.length-1)/2;
-        if(nums3.length%2!=0){
-            return (double)nums3[mid];
+        Arrays.sort(nums3);
+        if(nums3.length%2==1){
+            return (double)nums3[(nums3.length-1)/2];
         }
-        else{
-            return (double)(nums3[mid]+nums3[mid+1])/2;
-        
-        }
-
-        // int start = 0;
-        // int end = nums3.length-1;
-        // while(start<=end){
-        //     double mid = start +(end - start)/2;
-        //     double sum = 0;
-        //     if(nums3.length%2!=0){
-        //         return nums3[mid];
-        //     }
-        //     else if(nums3.length%2==0){
-        //         sum = (nums3[mid]+nums3[mid-1])/2;
-        //         return sum;
-        //     }
-        //     else{
-        //         continue;
-        //     }
-        // }
-
-        // return 0;
+        return (double) (nums3[(nums3.length)/2]+nums3[((nums3.length)/2)-1])/2;
     }
 }
